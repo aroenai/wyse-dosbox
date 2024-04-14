c0699dc46c5934cf358c10c1e3bd17085c81b99b
aroenai <28711790+aroenai@users.noreply.github.com>
2024-04-14 16:05:49 -0600

    Update menu.sh with arrays
    Simplify expanding the menu by using arrays.

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -3,94 +3,89 @@
 DIALOG_CANCEL=1
 DIALOG_ESC=255
 HEIGHT=0
-WIDTH=0
+WIDTH=50
+_menutitle="Main Menu"
 _backtitle="Dell Wyse 3040"
 
+# Define the menu labels and command(s) to run separated by a comma
+main_menu=(
+  "Start DOSBOX,_launch dosbox"
+  "Start ScummVM,_launch scummvm"
+  "Options,_options"
+  "Shutdown,sudo shutdown -h now"
+)
+
+options_menu=(
+  "Sound Mixer,alsamixer"
+  "Configure DOSBOX,nano $HOME/.config/dosbox/dosbox-staging.conf"
+  "Configure ScummVM,nano $HOME/.config/scummvm/scummvm.ini"
+  "Reboot,sudo shutdown -r now"
+  "Shutdown,sudo shutdown -h now"
+)
+
 _launch () {
-  echo $@ > $HOME/._openapp
+  # Parameter can be executable from $PATH or full path
+  echo $@ > "$HOME/._openapp"
   if [ $(tty) == "/dev/tty1" ]; then
     startx -- >/dev/null 2>&1
   fi
 }
 
-_options () {
-  while true; do
-    exec 3>&1
-    selection=$(dialog \
-      --backtitle "$_backtitle" \
-      --title "Options" \
-      --clear \
-      --cancel-label "Back" \
-      --menu "Please select:" $HEIGHT $WIDTH 4 \
-      "1" "Sound Mixer" \
-      "2" "Configure DOSBOX" \
-      "3" "Reboot" \
-      "4" "Shutdown" \
-      2>&1 1>&3)
-    exit_status=$?
-    exec 3>&-
-    case $exit_status in
-      $DIALOG_CANCEL | $DIALOG_ESC )
-        clear
-        break
-        ;;
-    esac
-    case $selection in
-      1 )
-        alsamixer
-        ;;
-      2 )
-        nano $HOME/.config/dosbox/dosbox-staging.conf
-        ;;
-      3 )
-        sudo shutdown -r now
-        ;;
-      4 )
-        sudo shutdown -h now
-        ;;
-    esac
+_dialog () {
+  local menu_items=("$@")
+  declare -i count=${#menu_items[@]}
+  declare -i i=0
+  local menu=()
+  local action=()
+  if [ "$_menutitle" == "Main Menu" ]
+    then
+    # Set the action for exiting the main menu
+    local _cancellabel="Exit"
+    local _cancelaction="echo 'Return to Linux shell' && exit"
+	else
+    # Set the action for any sub menus
+    local _cancellabel="Back"
+    local _cancelaction="break"
+  fi
+  for row in "${menu_items[@]}"; do
+  menu+=("$(expr ${i} + 1)")
+  menu+=("$(echo "$row" | awk -F',' '{ print $1 }')")
+  action[$i]=$(echo "$row" | awk -F',' '{ print $2 }')
+  i+=1
   done
-}
-
-while true; do
+  while true; do
   # Deplicate (make a backup copy of) file descriptor 1 (stdout)
   # on descriptor 3
   exec 3>&1
   # Generate the dialog box while running dialog in a subshell
   selection=$(dialog \
-    --backtitle "$_backtitle" \
-    --title "Main Menu" \
-    --clear \
-    --cancel-label "Exit" \
-    --menu "Please select:" $HEIGHT $WIDTH 4 \
-    "1" "Start DOSBOX" \
-    "2" "Start ScummVM" \
-    "3" "Options" \
-    "4" "Shutdown" \
-    2>&1 1>&3)
+      --backtitle "$_backtitle" \
+      --title "$_menutitle" \
+      --clear \
+      --cancel-label "$_cancellabel" \
+      --menu "Please select:" $HEIGHT $WIDTH $count \
+      "${menu[@]}" \
+      2>&1 1>&3)
   # Get dialog's exit status
-  exit_status=$?
+    exit_status=$?
   # Close file descriptor 3
-  exec 3>&-
-  case  $exit_status in
-    $DIALOG_CANCEL | $DIALOG_ESC )
-      clear
-      echo "Return to linux shell."
-      exit
-      ;;
-  esac
-  case $selection in
-    1 )
-      _launch dosbox
-      ;;
-    2 )
-      _launch scummvm
-      ;;
-    3 )
-      _options
-      ;;
-    4 )
-      sudo shutdown -h now
-      ;;
-  esac
-done
+    exec 3>&-
+    case $exit_status in
+      $DIALOG_CANCEL | DIALOG_ESC )
+        clear
+        eval "${_cancelaction}"
+        ;;
+    esac
+    eval "${action[$(expr ${selection} - 1 )]}"
+  done
+}
+
+_options () {
+  # Set the menu title for the new dialog
+  _menutitle="Options"
+  _dialog "${options_menu[@]}"
+  # Set the menu title for the previous dialog
+  _menutitle="Main Menu"
+}
+
+_dialog "${main_menu[@]}"
