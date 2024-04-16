029fbe8de584a5b1bda4837ffe5a9856180fb970
aroenai <28711790+aroenai@users.noreply.github.com>
2024-04-15 22:47:15 -0600

    Update menu.sh
    Improve logic and cleanup spacing issues

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -4,11 +4,11 @@ DIALOG_CANCEL=1
 DIALOG_ESC=255
 HEIGHT=0
 WIDTH=50
-_menutitle="Main Menu"
 _backtitle="Dell Wyse 3040"
 
-# Define the menu labels and command(s) to run separated by a comma
+# Define the menu title, labels and command(s) to run separated by a comma
 main_menu=(
+  "Main Menu"
   "Start DOSBOX,_launch dosbox"
   "Start ScummVM,_launch scummvm"
   "Options,_options"
@@ -16,12 +16,26 @@ main_menu=(
 )
 
 options_menu=(
+  "Options"
   "Sound Mixer,alsamixer"
   "Configure DOSBOX,nano $HOME/.config/dosbox/dosbox-staging.conf"
   "Configure ScummVM,nano $HOME/.config/scummvm/scummvm.ini"
   "Reboot,sudo shutdown -r now"
   "Shutdown,sudo shutdown -h now"
 )
+
+_t () {
+  type -p "$1" &>/dev/null;
+}
+
+_which_cmd () {
+  _t dialog && DIA=dialog && DIA_CANCEL="--cancel-label" && return
+  _t whiptail && DIA=whiptail && DIA_CANCEL="--cancel-button" && return
+  if [ -z $DIA ]; then
+    echo "No whiptail or dialog found."
+    exit 1
+  fi
+}
 
 _launch () {
   # Parameter can be executable from $PATH or full path
@@ -33,59 +47,63 @@ _launch () {
 
 _dialog () {
   local menu_items=("$@")
-  declare -i count=${#menu_items[@]}
   declare -i i=0
-  local menu=()
-  local action=()
-  if [ "$_menutitle" == "Main Menu" ]
-    then
-    # Set the action for exiting the main menu
-    local _cancellabel="Exit"
-    local _cancelaction="echo 'Return to Linux shell' && exit"
-	else
-    # Set the action for any sub menus
-    local _cancellabel="Back"
-    local _cancelaction="break"
-  fi
+  declare -a menu action
   for row in "${menu_items[@]}"; do
-  menu+=("$(expr ${i} + 1)")
-  menu+=("$(echo "$row" | awk -F',' '{ print $1 }')")
-  action[$i]=$(echo "$row" | awk -F',' '{ print $2 }')
-  i+=1
+    if [ $i == 0 ]
+    then
+      _menutitle="$row"
+      if [ "$_menutitle" == "${main_menu[0]}" ]
+      then
+        # Set the action for exiting the main menu
+        local _cancellabel="Exit"
+        local _cancelaction="echo 'Return to Linux shell' && exit"
+      else
+        # Set the action for any sub menus
+        local _cancellabel="Back"
+        local _cancelaction="break"
+      fi
+    else
+      menu+=("$(expr ${i})")
+      menu+=("$(echo "$row" | awk -F',' '{ print $1 }')")
+      action[$i]=$(echo "$row" | awk -F',' '{ print $2 }')
+    fi
+    i+=1
   done
   while true; do
-  # Deplicate (make a backup copy of) file descriptor 1 (stdout)
-  # on descriptor 3
-  exec 3>&1
-  # Generate the dialog box while running dialog in a subshell
-  selection=$(dialog \
+    # Deplicate (make a backup copy of) file descriptor 1 (stdout)
+    # on descriptor 3
+    exec 3>&1
+    # Generate the dialog box while running dialog in a subshell
+    selection=$("$DIA" \
       --backtitle "$_backtitle" \
       --title "$_menutitle" \
       --clear \
-      --cancel-label "$_cancellabel" \
-      --menu "Please select:" $HEIGHT $WIDTH $count \
+      "$DIA_CANCEL" "$_cancellabel" \
+      --menu "Please select:" $HEIGHT $WIDTH $(expr ${i} - 1) \
       "${menu[@]}" \
       2>&1 1>&3)
-  # Get dialog's exit status
+    # Get dialog's exit status
     exit_status=$?
-  # Close file descriptor 3
+    # Close file descriptor 3
     exec 3>&-
     case $exit_status in
       $DIALOG_CANCEL | $DIALOG_ESC )
         clear
         eval "${_cancelaction}"
         ;;
+      0 )
+        eval "${action[${selection}]}"
+        ;;
     esac
-    eval "${action[$(expr ${selection} - 1 )]}"
   done
 }
 
 _options () {
-  # Set the menu title for the new dialog
-  _menutitle="Options"
   _dialog "${options_menu[@]}"
   # Set the menu title for the previous dialog
-  _menutitle="Main Menu"
+  _menutitle=${main_menu[0]}
 }
 
+_which_cmd
 _dialog "${main_menu[@]}"
